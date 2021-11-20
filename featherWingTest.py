06fa9276ef8cbcd94f85626c415f69b46e43dd72
cakeisdead <1744036+cakeisdead@users.noreply.github.com>
2021-11-20 12:19:14 -0600

    Update featherWingTest.py
    pep8 modifications

--- featherWingTest.py
+++ featherWingTest.py
@@ -3,19 +3,19 @@ import board
 import neopixel
 
 # NeoPixel parameters
-led_count = 32 # Number of LED pixels - The FeatherWing has 32 leds
-data_pin = board.D18 # GPIO PI - We are using GPIO 18 to communicate with the FeatherWing
-led_brightness = 1  # 1 is REALLY bright
-aw = False      # auto_write
-color_order = neopixel.RGB # this is the color channel order. (GRB, GRBW, RGB or RGBW)
+LED_COUNT = 32 # Number of LED pixels - The FeatherWing has 32 leds
+DATA_PIN = board.D18 # GPIO PI - We are using GPIO 18 to communicate with the FeatherWing
+LED_BRIGHTNESS = 1  # 1 is REALLY bright
+AW = False      # auto_write
+COLOR_ORDER = neopixel.RGB # this is the color channel order. (GRB, GRBW, RGB or RGBW)
 
 # Color Settings (RGB values)
-colorNone = (0,0,0)
-colorMoving = (6,245,42)
-colorFinal = (235,106,71)
+color_none = (0,0,0)
+color_moving = (6,245,42)
+color_final = (235,106,71)
 
 # Create NeoPixel object with appropriate configuration.
-fw = neopixel.NeoPixel(data_pin, led_count, brightness = led_brightness, auto_write=aw, pixel_order = color_order)
+fw = neopixel.NeoPixel(DATA_PIN, LED_COUNT, brightness = LED_BRIGHTNESS, auto_write=AW, pixel_order = COLOR_ORDER)
 
 
 def putPixel(position, color):
@@ -26,8 +26,8 @@ final = LED_COUNT
 
 while final >= 0:
     for led in range(final):
-        putPixel(led,colorMoving)
+        putPixel(led,color_moving)
         time.sleep(0.1)
-        putPixel(led,colorNone)
-    putPixel(final-1, colorFinal)
+        putPixel(led,color_none)
+    putPixel(final-1, color_final)
     final -= 1
